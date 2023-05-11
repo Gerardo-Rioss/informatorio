5d2dc6fe4ef410b702f5ec0e1f7e312b05deaab9
Ríos <grios@insssep.gov.ar>
2023-05-11 12:29:16 -0300

    18

--- EJERCICIOS/semana4funciones.py
+++ EJERCICIOS/semana4funciones.py
@@ -125,5 +125,12 @@ print(eliminar_duplicados([100,30,50,100,300,350,40,350])) """
     else:
         return False """
 
+# 18
+""" def calcular_mayor_diferencia(numeros):
+    return max(numeros)-min(numeros)
+print(calcular_mayor_diferencia([100,30,15,150,2])) """
 
+# 19
+def es_bisiesto(anio):
+    
 
