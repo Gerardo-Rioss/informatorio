cd83edd0ebc9559997e231ee4dbdef1e3ac83fda
gerardo-rioss <grios@insssep.gov.ar>
2023-05-16 08:16:57 -0300

    validaciones

--- DESAFIOS/desafio4.py
+++ DESAFIOS/desafio4.py
@@ -4,19 +4,39 @@ lista_inmuebles = {'año': 2010, 'metros': 150, 'habitaciones': 4, 'garaje': Tru
 {'año': 2015, 'metros': 95, 'habitaciones': 3, 'garaje': True, 'zona': 'B', 'estado': 'Vendido'},
 {'año': 2008, 'metros': 60, 'habitaciones': 2, 'garaje': False, 'zona': 'C', 'estado': 'Disponible'}
 
-datos_correctos = {"correcto" : False}
 zonas=("A","B","C")
 
+#                             DECLARACION  DE FUNCIONES:
 
-def anio_correcto():
-    while true:
-        anio = input("Año: ")
-        if len(anio) !=4:
-            print("El año tiene que tener cuatro digitos, intentelo de nuevo")
-            continue
-        if not anio.isdigit():
-            print("El año debe ser solo numeros. Intetelo de nuevo")
-            continue
-        break
+# VALIDACION DE DATOS INGRESADOS:
 
-print (anio_correcto())
+# funcion para validad que los datos ingresados en AÑO sea un valor entero de 4 digitos y controle que no ingrese vacios 
+def validar_anio(anio):
+    if len(anio)!=4 or not anio.isdigit() or anio == "" or anio.isspace():
+        return False
+    return True
+# funcion para validad que los datos ingresados en los METROS que sean enteros y que no ingrese vacios 
+def validar_metros(metros):
+    if not metros.isdigit() or metros == "" or metros.isspace():
+        return False
+    return True
+# funcion para validad que los datos ingresados en HABITACIONES que sean enteros y que no ingrese vacios 
+def validar_habitaciones(habitaciones):
+    if not habitaciones.isdigit() or habitaciones =="" or habitaciones.isspace():
+        return False
+    return True
+# funcion para validad que los datos ingresados en GARAJE que sean enteros y que no ingrese vacios (solo puede ingresar 1 o 2) 
+def validar_garaje(garaje):
+    opciones ="12"
+    if not garaje.isdigit() or garaje =="" or garaje.isspace() or garaje not in opciones:
+        return False
+    return True
+
+def validar_zona(zona):
+    opciones ="ABC"
+    if not zona.isdigit() or zona =="" or zona.isspace() or (zona.upper() not in opciones):
+        return False
+    return True
+
+
+print(validar_zona("a"))
