b0b0caa404d10cd8476680f2409b1ab39c8804d6
Rios Gerardo <gerarios26@gmail.com>
2023-05-17 16:39:51 -0300

    copia

--- DESAFIOS/desafio4.py
+++ DESAFIOS/desafio4.py
@@ -11,16 +11,16 @@ lista_inmuebles = [
 def validar_anio(anio):
     # si el dato de entrada es anio
     if anio =='' or anio.isspace()== True:
-        print('ERROR, no puede contener espacios en blanco...')
+        print('ERROR, no puede contener espacios en blanco!!!')
         return False
     elif anio.isdigit() == False:
-        print('ERROR, tiene que ser un numero entero...')    
+        print('ERROR, tiene que ser un numero entero!!!')    
         return False
     elif len(anio)!=4: 
-        print('ERROR, tiene que ser un numero de cuatro digitos...')
+        print('ERROR, tiene que ser un numero de cuatro digitos!!!')
         return False
     elif int(anio)<2000:
-        print('ERROR, el inmueble debe ser mayor al año 2000...')
+        print('ERROR, el inmueble debe ser mayor al año 2000!!!')
         return False
     return True
 
@@ -30,7 +30,7 @@ def validar_metros(metros):
         print('ERROR, no puede contener espacios en blanco...')
         return False
     elif metros.isdigit()==False: 
-        print('ERROR, tiene que ser un numero entero...')
+        print('ERROR, tiene que ser un numero entero!!!')
         return False
     elif int(metros)<60:
         print('ERROR, el inmueble no puede tener menos de 60 metros cuadrados...')
@@ -40,42 +40,42 @@ def validar_metros(metros):
 # HABITACIONES
 def validar_habitaciones(habitaciones):
     if habitaciones =='' or habitaciones.isspace()==True:
-        print('ERROR, no puede contener espacios en blanco...')
+        print('ERROR, no puede contener espacios en blanco!!!')
         return False
     elif habitaciones.isdigit() == False:
-        print('ERROR, tiene que ser un numero entero...')
+        print('ERROR, tiene que ser un numero entero!!!')
         return False
     elif int(habitaciones)<2:
-        print('ERROR, el inmueble no puede tener menos de dos habitaciones...')
+        print('ERROR, el inmueble no puede tener menos de dos habitaciones!!!')
         return False
     return True
 
 # GARAJE
 def validar_garaje(garaje):
     if garaje =='' or garaje.isspace() == True:
-        print('ERROR, no puede contener espacios en blanco...')
+        print('ERROR, no puede contener espacios en blanco!!!')
         return False
     elif garaje not in ('1','2'):
-        print('ERROR, los valores que debe ingresar son: 1 = SI / 2 = NO ...')
+        print('ERROR, los valores que debe ingresar son: 1 = SI / 2 = NO !!!')
         return False
     return True
 
 # ZONA
 def validar_zonas(zona):
     if zona =='' or zona.isspace()==True:
-        print('ERROR, no puede contener espacios en blanco...')
+        print('ERROR, no puede contener espacios en blanco!!!')
         return False
     elif zona.upper() not in ('A','B','C'):
-        print('ERROR, tiene que elegir unas de las zonas: A - B - C ...' )
+        print('ERROR, tiene que elegir unas de las zonas: A - B - C !!!' )
         return False
     return True
 
 # ESTADO
 def validar_estado(estado):
     if estado =='' or estado.isspace():
-        print('ERROR, no puede contener espacios en blanco...')
+        print('ERROR, no puede contener espacios en blanco!!!')
     elif estado not in ('1','2','3'):
-        print('ERROR, Ingresa el valor segun correponda al estado del inmueble: 1 - Disponible / 2 - Reservado / 3 - Vendido...')
+        print('ERROR, Ingresa el valor segun correponda al estado del inmueble: 1 - Disponible / 2 - Reservado / 3 - Vendido!!!')
         return False
     return True
 
@@ -128,11 +128,25 @@ def imprimir_lista():
     for inmueble in lista_inmuebles:
         print(inmueble)
 
-# FUNCION PARA AGREGAR UN INMUEBLE NUEVO
+# FUNCION AGREGAR UN INMUEBLE
 def agregar_inmueble(nuevo_inmueble):
     lista_inmuebles.append(nuevo_inmueble)
 
+# FUNCION EDITAR INMUEBLE
+def editar_inmueble(inmueble_a_editar):
+    if inmueble_a_editar in lista_inmuebles:
+        print('        Ingrese los datos del inmueble con las modificaciones correspondientes:')
+        index_del_inmueble = lista_inmuebles.index(inmueble_a_editar)
+        inmueble_modificado = ingresar_inmueble()
+        lista_inmuebles.index(index_del_inmueble)==inmueble_modificado
+    else:
+        print('ERROR - El inmueble ingresado no se encuentra ingresado al sistema!!!')
 
+    
+
+imprimir_lista()
+print(editar_inmueble(ingresar_inmueble()))
+imprimir_lista()
 
 
 
